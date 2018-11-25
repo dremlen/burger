ca6be8ec28630f3e99d77fd1380231585e36d474
Dmitriy Sidletskiy <dremlen23@yandex.ru>
2018-11-25 18:16:15 +0400

    form-js

--- app/js/script.js
+++ app/js/script.js
@@ -27,7 +27,8 @@ menuLink.addEventListener('click', function(event){
   mobMenu.style.display = 'block';
 });
 
-close.addEventListener('click', function(){
+close.addEventListener('click', function(event){
+  event.preventDefault();
   mobMenu.style.display = 'none';
 });
 
@@ -96,3 +97,70 @@ left.addEventListener('click', function(e){
   }
   slaids.style.right = currentRight + "vw";
 });
+
+
+// Отправка формы
+
+const form = document.querySelector('.form');
+const orderBtn = document.querySelector('.form__btn');
+
+orderBtn.addEventListener('click', function(event){
+  event.preventDefault();
+
+  if(validateFofm(form)){
+    const data = {
+      name:form.elements.name.value,
+      tel:form.elements.tel.value,
+      street: form.elements.street.value,
+      house: form.elements.house.value,
+      housing: form.elements.housing.value,
+      room: form.elements.room.value,
+      floor: form.elements.floor.value,
+      comment: form.elements.comment.value
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+    xhr.send(JSON.stringify(data));
+    xhr.addEventListener('load', function(){
+      const modalForm = document.querySelector('.modal');
+      const modBtn = document.querySelector('.modal__btn');
+      modalForm.style.display ='flex';
+      modBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        modalForm.style.display = 'none';
+      }) 
+       });
+    }
+});
+
+function validateFofm(form){
+  let valid = true;
+
+  if(!validateFild(form.elements.name)){
+    valid = false;
+  }
+  if(!validateFild(form.elements.tel)){
+    valid = false;
+  }
+  if(!validateFild(form.elements.street)){
+    valid = false;
+  }
+  if(!validateFild(form.elements.house)){
+    valid = false;
+  }
+  if(!validateFild(form.elements.room)){
+    valid = false;
+  }
+  return valid;
+}
+function validateFild(blocks){
+  if(!blocks.checkValidity()){
+    blocks.nextElementSibling.textContent = blocks.validationMessage;
+    return false;
+  }
+    else{
+      blocks.nextElementSibling.textContent = '';
+      return true;
+    }
+}
